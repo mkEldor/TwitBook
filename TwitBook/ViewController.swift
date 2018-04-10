da7a0d0cacd659dfa7acb48f65618bd2ad41e6c9
Эльдар Рахимов <eldor@eldors-macbook-air.local>
2018-04-10 08:35:09 +0600

    Initial Commit

---
+++ TwitBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitBook
+//
+//  Created by Eldor Makkambaev on 10.04.2018.
+//  Copyright © 2018 Eldor Makkambaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
